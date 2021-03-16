ea1e0991b93e71b81d42c65ba4b2fcd65ba9de04
gnourt2112000 <74771782+gnourt2112000@users.noreply.github.com>
2021-03-16 23:23:45 +0700

    Create DataTest.java

---
+++ Lab03/AimsProject/DataTest.java
@@ -0,0 +1,8 @@
+public class DataTest {
+    public static void main(String[] args) {
+        MyDate md = new MyDate();
+        md.accept();
+        md.getDate();
+        md.printCurrentDate();
+    }
+}
